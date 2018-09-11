e16a28b705e099fe34b91b12f198e88cb2ef1589
gongxia.chen <chengongxia1990@gmail.com>
2018-09-11 02:38:32 -0400

    add material-UI dependency, and demo of sidebar

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Sidebar from './sidebar';
+import '../App.css';
+
+const App = () => (
+  <div className="App">
+    <Sidebar />
+  </div>
+);
+
+export default App;
