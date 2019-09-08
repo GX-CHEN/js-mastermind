c5d3d2debc3c8e3917d6ddabdb9b804e20d8b0c4
Gongxia Chen <chengongxia1990@gmail.com>
2019-09-08 18:22:51 -0400

    deploy: add new assets for deploy the repl.it uri refactor

---
+++ build/precache-manifest.a03613a1c59f676ed9ce78fc9b4d1c92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c58390f9e3ab8739080680ee7727e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d65a50361d74156545d0",
+    "url": "/static/css/main.c620caa9.chunk.css"
+  },
+  {
+    "revision": "a33da7c962c1298dcf60",
+    "url": "/static/js/2.04c0244e.chunk.js"
+  },
+  {
+    "revision": "d65a50361d74156545d0",
+    "url": "/static/js/main.c04b6b85.chunk.js"
+  },
+  {
+    "revision": "de580a95fc5afeca68d3",
+    "url": "/static/js/runtime~main.369f70b0.js"
+  }
+]);
